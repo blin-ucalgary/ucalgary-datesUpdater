b53b9f208afb8005b18e4f4e88e3997d53cb0bc9
Benny Lin <blin@ucalgary.ca>
2012-01-12 14:35:03 -0700

    Added base rails files

---
+++ dates_updater/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatesUpdater::Application.config.secret_token = '8c95239656c7c869d5f2897bbfcfaaab29bd7c05e2534f2e56be6347b72771050f25644b96beb43d0f316f739337a5e0346cefdb2cb0a86ad1d88e8979b27961'
