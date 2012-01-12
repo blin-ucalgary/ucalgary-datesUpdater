cf87ea5ad062cf0ccacbe712ce7c49a05a06831a
Benny Lin <blin@ucalgary.ca>
2012-01-12 16:07:08 -0700

    Removed bad migration

--- dates_updater/db/migrate/20120112221819_create_important_dates_tags.rb
+++
@@ -1,6 +0,0 @@
-class CreateImportantDatesTags < ActiveRecord::Migration
-  create_table :important_dates_tags, :id => false do |t|
-    t.integer :important_date_id
-    t.integer :tag_id
-  end
-end
