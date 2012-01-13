fec5d8391153d9d473d9741304f960341e560afb
Benny Lin <blin@ucalgary.ca>
2012-01-13 14:53:37 -0700

    Added hook to downcase tag names for consistency

--- dates_updater/app/models/tag.rb
+++ dates_updater/app/models/tag.rb
@@ -1,10 +1,18 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :important_dates
+  
+  before_save :downcase_tag_name
 
   def as_json(options={})
     {
       :name => self.name
     }
+  end
+
+  protected
+
+  def downcase_tag_name
+    self.name.downcase!
   end
 
 end
